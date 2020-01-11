7ecc010b1306ca1144417c40090c5b47ac2ab29e
Anirudh Gupta <anirudhgupta281998@gmail.com>
2020-01-11 19:26:45 +0530

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const path = require('path')
-const socketio = require('../lib/socket.io')
+const socketio = require('socket.io')
 const http = require('http')
 
 const app = express()
